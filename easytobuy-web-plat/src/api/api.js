4b6cc157bbfae2245b4b5dde62975349fa779503
xxc9830 <1258345660@qq.com>
2019-02-14 21:56:31 +0800

    修改3

--- easytobuy-web-plat/src/api/api.js
+++ easytobuy-web-plat/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let base = 'https://www.easy-mock.com/mock/5c511af2cc522243103f0a3c';
+let base = '';
 
 export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
 
